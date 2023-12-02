1f95e77c6385fb89cadfa61b59682e835c3b88ba
Ирина Баранова <irina.baranova14@outlook.com>
2023-12-02 15:42:32 +0300

    Добавьте файлы проекта.

---
+++ Задача 7.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7._1
+{
+    internal class Program
+    {
+        static double GetgetTriangle(double a, double b, double c)
+        {
+            double p = (a + b + c)/2;
+            double S =  Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите длины первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = GetgetTriangle(x1, y1, z1);
+            Console.WriteLine("Ведите длины второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = GetgetTriangle(x2, y2, z2);
+
+            if(c1>c2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if(c1<c2)
+                Console.WriteLine("Площадь первого треугольника меньше");
+            else
+                Console.WriteLine("Площадь треугольников равны");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Задача 7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7._2
+{
+    internal class Program
+    {
+        static void GetCube(double l, out double s, out double v)
+        {
+            s = 6 * l *l;
+            v = l * l * l;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double l = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetCube(l, out s, out v);
+            Console.WriteLine("{0:.00} {1:.00}", s, v);
+            Console.ReadKey();
+        }
+    }
+}
